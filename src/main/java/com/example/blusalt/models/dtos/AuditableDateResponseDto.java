f19ceb0ab3c9f4ab8e1d14481bca589926b080ba
Oluseyi Jonah <oluseyi@USERs-MacBook-Pro.local>
2026-03-04 09:47:45 +0100

    correction

--- src/main/java/com/example/blusalt/models/dtos/AuditableDateResponseDto.java
+++ src/main/java/com/example/blusalt/models/dtos/AuditableDateResponseDto.java
@@ -1,7 +1,12 @@
 package com.example.blusalt.models.dtos;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
 public class AuditableDateResponseDto {
     private LocalDateTime createdDate;
     private LocalDateTime updatedDate;
